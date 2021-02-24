ce47aad25032c61387e36f3f238f791fe5ec841d
unknown <sha082072@gmail.com>
2021-02-25 00:25:23 +0900

    2/25

--- 백준 알고리즘/백준 1157/1157.py
+++ 백준 알고리즘/백준 1157/1157.py
@@ -1,25 +1,23 @@
-# 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램
-
 import sys
 sys.stdin = open("input.txt")
 
-word = (input()).upper()
+word = (input()).upper()  # 입력받은 단어를 대문자로 변환
 
+dict1 = dict() # 가장 많이 사용된 알파벳 알아내기 위한 딕셔너리 정의
 
-dict1 = dict() # 가장 많이 사용된 알파벳 알아내기 위한 딕셔너리
 for i in word :
    if i in dict1 :
         dict1[i] +=1
    else:
         dict1[i] =1
 
-m = max(dict1.values())
-cnt =0
-for key, i in dict1.items():
-    if i == m :
+m = max(dict1.values()) #딕셔너리 값 중에서 최대값을 정의
+cnt =0 # 최댓값이 나오는 횟수
+for key, value in dict1.items():
+    if value == m :
         cnt += 1
         j = key
-if cnt > 1:
+if cnt > 1: # 최대값의 횟수가 1보다 큰 경우 ? 출력
     print("?")
 else :
     print(j)
--- 백준 알고리즘/백준 2999/2999.py
+++ 백준 알고리즘/백준 2999/2999.py
@@ -2,25 +2,24 @@ import sys
 sys.stdin = open("input.txt")
 
 alpha = input()
-num = len(alpha)
-list1 =[]
-
-for i in range(int(num**0.5),0,-1):
-    if num % i == 0 :
-        row = i
-        col = num // i
-        break
-arr = [[0 for _ in range(col)] for _ in range(row)]
-
 cnt =0
 
+# 입력받은 문자열의 길이를 사용해서 행렬의 가로 세로 크기를 측정하기
+for i in range(int(len(alpha)**0.5),0,-1):
+    if len(alpha) % i == 0 :
+        row = i
+        col = len(alpha)// i
+        break
+# 2차원 리스트 0 초기화
+arr = [[0 for _ in range(col)] for _ in range(row)]
 
+# 2차원 리스트에 문자열을 각 칸 마다 대입하기
 for i in range(col) :
     for j in range(row) :
         arr[j][i] = alpha[cnt]
         cnt +=1
 
-
+# 행렬의 문자들을 합쳐서 문자열을 만든다
 answer = ''
 for brr in arr:
    answer+= ''.join(brr)
