279504381267040a4402afbbd8ddf9bdf08ceb41
unknown <sha082072@gmail.com>
2021-03-01 20:51:42 +0900

    3/1

--- SW_Expert_Academy_Algorithm/1206/1157.py
+++ SW_Expert_Academy_Algorithm/1206/1157.py
@@ -1,3 +1,4 @@
+
 import sys
 sys.stdin = open("input.txt")
 
@@ -10,3 +11,9 @@ for j in range(10) : #테스트케이스 10번
              cnt += min(arr[i]-arr[i+1],arr[i]-arr[i-1],arr[i]-arr[i+2],arr[i]-arr[i-2])
 
     print("#{} {}".format(j+1,cnt))
+
+
+# 이중 for문과 min()을 이용하기 때문에 시간 복잡도 : O(n^2) + O(n) = n^2
+# 10 * 1000
+# 10번의 테스트 케이스를 반복하면서 각 테스트 케이스마다 가지는 길이를 인덱스로 사용해서 조망권이 확보된 세대의 수를 찾았다
+# 왼쪽 오른쪽 2 칸 이내에 있는 건물은 반드시 자신의 건물 최대 높이 보다 낮아야 한다
--- 백준 알고리즘/백준 9012/9012.py
+++ 백준 알고리즘/백준 9012/9012.py
@@ -7,7 +7,7 @@ for _ in range(T) :
     str = input()
     for i in range(len(str)-1):
         if str.count("(") == str.count(")") and str[-1] ==")" and str[0] =="(" and str[:i].count("(") >= str[:i].count(")"):
-            isTrue = True #시작과 끝은 (), 절대로 이전의 문자열에서 )의 갯수가 더 많아서는 안된다
+            isTrue = True # 괄호 성립 조건 true
         else:
             isTrue = False
             break
@@ -15,3 +15,6 @@ for _ in range(T) :
         print("YES")
     else :
         print("NO")
+
+# 시간복잡도 : 이중 for문을 사용하면서 .count() 함수를 사용하기 때문에 O(n^2) + O(n) = n^2 이 된다.
+# 시작과 끝은 ()이고, 절대로 이전의 문자열에서 )의 갯수가 더 많아서는 안된다
