5236039710111f6a6d6e33686746463925048b30
unknown <sha082072@gmail.com>
2021-03-04 19:05:05 +0900

    3/4

--- SW_Expert_Academy_Algorithm/1209/1209.py
+++ SW_Expert_Academy_Algorithm/1209/1209.py
@@ -5,20 +5,29 @@ sys.stdin = open("input.txt")
 
 for j in range(10):
     num=int(input())
-    large = []
-    maximum = []
+    sum1 = 0
+    sum2 = 0
+    sum3 =0
+    sum4 = 0
+    large = [] #2차원 행렬
+    maximum = [] # 대각선, 가로, 세로의 각 합에 대한 리스트
     for i in range(100):
         small=input()
         n_small = list(map(int,small.split()))
         large.append(n_small)
-    #print(large)
+
     for i in range(100):
-        sum=0
-        sum2=0
-        sum += large[i][i]
-        maximum.append(sum)
-        sum2 += large[i][101-i]
-    print("대각선: " + sum)
-    # for i in range(100):
-    #     sum = 0
-    #     sum +=large[i]
+        sum1 += large[i][i]
+        sum2 += large[i][99-i]
+        for j in range(100):
+            sum3+=large[0][j]
+
+            sum4+=large[j][0]
+        maximum.append(sum3)
+        maximum.append(sum4)
+    maximum.append(sum1)
+    maximum.append(sum2)
+
+    print(maximum)
+
+
