bb7ea236ff0ce9fb7e7909d7e97fd07ac224e089
unknown <sha082072@gmail.com>
2021-03-08 23:51:31 +0900

    3/8

---
+++ 파이썬 알고리즘 인터뷰/Trapping Rain Water.py
@@ -0,0 +1,12 @@
+# 높이를 입력받아 비 온 후 얼마나 많은 물이 쌓일 수 있는지 계산하라
+
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# 0 ~ 11 인덱스 존재
+
+def findRain(heght_list):
+    water = [] # 쌓일 수 있는 물
+    for i in range(1,len(heght_list)-1):
+        if water and heght_list[i]
+
+        elif:
+findRain(height)
