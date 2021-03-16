32f006f127aa09449f028ea3ad82c266fb6aaabf
unknown <sha082072@gmail.com>
2021-03-16 23:54:24 +0900

    3/16

--- 파이썬 알고리즘 인터뷰/Daily Temporature.py
+++ 파이썬 알고리즘 인터뷰/Daily Temporature.py
@@ -2,7 +2,7 @@ T = [73,74,75,71,69,72,76,73]
 day_list =[]
 for index, value in enumerate(T):
     count = []
-    for i in range(index+1,len(T)):
+    for i in range(index+1,len(T)-1):
         count.append(T[i])
         if value >= T[i]:
             continue
@@ -10,3 +10,18 @@ for index, value in enumerate(T):
             break
     day_list.append(len(count))
 print(day_list)
+
+
+def dailyTemporatures(T):
+    answer = [0] * len(T)
+    stack =[]
+    for index,cur_value in enumerate(T):
+        # 현재 온도가 스택 값보다 높다면 정답 처리
+        while stack and cur_value >T[stack[-1]]: # T 이렇게 표시하는게 맞나?
+            last = stack.pop()
+            answer[last] = index-last
+        stack.append(index)
+
+    return answer
+
+print(dailyTemporatures(T))
