b99eae1085e11f8b53a7ef20d837712ab05da5f7
unknown <sha082072@gmail.com>
2021-03-22 18:59:13 +0900

    3/22

--- 파이썬 알고리즘 인터뷰/BFS.py
+++ 파이썬 알고리즘 인터뷰/BFS.py
@@ -18,18 +18,18 @@ def bfs(graph, start, visited):
                 visited[i]= True
 
                 # 2차원 리스트
-                graph = [
-                    [],
-                    [2, 3, 8],
-                    [1, 7],
-                    [1, 4, 5],
-                    [3, 5],
-                    [3, 4],
-                    [7],
-                    [2, 6, 8],
-                    [1, 7]
-                ]
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
 
-                # 각 노드가 방문된 정보를 표현
-                visited = [False] * 9
-                bfs(graph, 1, visited)
+# 각 노드가 방문된 정보를 표현
+visited = [False] * 9
+bfs(graph, 1, visited)
