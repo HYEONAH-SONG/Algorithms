3ca4a397b93dbef6ba8f33d6b2e7657e9604d720
unknown <sha082072@gmail.com>
2021-03-24 00:35:31 +0900

    3/24

--- 파이썬 알고리즘 인터뷰/Daily Temporature2.py
+++ 파이썬 알고리즘 인터뷰/Daily Temporature2.py
@@ -1,10 +1,14 @@
 T = [73,74,75,71,69,72,76,73]
-day =[]
+day =[0]*len(T)
 stack= []
 for index,value in enumerate(T):
+    # 현재의 온도가 스택 값보다 높다면 정답 처리
     while stack and T[stack[-1]]<value:
         i = stack.pop()
-        day.append(index-i)
+        day[i] = index-i
     stack.append(index)
-
+    # stack에 index 쌓아두기
+    # 핵심은 온도가 떨어지면 stack에 저장하고
+    # 온도가 상승하면 해당 인댁스를 빼기
+    # 0 --> 1 --> 2 ---> 2, 3, 4 --> 2,5 ---> 2,5,6
 print(day)
