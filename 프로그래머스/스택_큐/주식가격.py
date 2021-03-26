bf9ae7d5759cfefd0efdb05f583d124cd57b206d
unknown <sha082072@gmail.com>
2021-03-27 01:07:05 +0900

    3/27

--- 프로그래머스/스택_큐/주식가격.py
+++ 프로그래머스/스택_큐/주식가격.py
@@ -5,10 +5,16 @@ def solution(prices):
     stack=[]
     answer = [0]*len(prices)
     for index,cur_price in enumerate(prices):
-        while stack and cur_price >= prices[stack[-1]]:
+        # 현재의 값이 이전의 값 보다 작은 경우에는 1초가 무조건 answer에 적힌다.
+        while stack and cur_price < prices[stack[-1]]:
             last = stack.pop()
-            answer[index]=index-last
-        stack.append(index)
+            answer[last]=index-last
+        stack.append(index) # 0,1,3,4
+    # 0,0,1,0,0 ---> 세번째만 자신 보다 가격이 떨어지는 날 존재
+    while stack:
+        second = stack.pop()
+        answer[second] = len(prices) - (second+1)
+
     return answer
 
 prices = [1, 2, 3, 2, 3]
