5bee1b0d0940eee924aa900cc1bf074ccfad27b7
unknown <sha082072@gmail.com>
2021-04-24 00:24:22 +0900

    4/24

--- Exam/중간고사.py
+++ Exam/중간고사.py
@@ -3,7 +3,8 @@ n = int(input())
 
 x=0
 for i in range(n):
-    for j in range(n+1):
+    for j in range(i+1,n):
         for k in range(j+1,n):
             x =x+1
+            print("i: {} j:{} k:{},x : {}".format(i,j,k,x))
 print(x)
