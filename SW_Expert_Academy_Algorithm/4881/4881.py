fee4ac19f584c7b4960f7cefaf73c42aec6d50ce
unknown <sha082072@gmail.com>
2021-04-30 09:38:33 +0900

    4/30

---
+++ SW_Expert_Academy_Algorithm/4881/4881.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("input.txt")
+
+case = int(input())
+for _ in range(case):
+    num = int(input())
+    matrix = [list(map(int,input().split())) for _ in range(num)]
+
+
+for i in range(case):
+    sum = 0
+    for j in range(case):
+        if ()
--- 백준 알고리즘/백준 10026/10026.py
+++ 백준 알고리즘/백준 10026/10026.py
@@ -2,11 +2,13 @@
 # 같은 색으로 이루어진 구역의 갯수를 구하기
 # 적록색약인 사람과 아닌 사람이 봤을 때 구역의 갯수 구하기
 
-import sys
+# import sys
 from collections import deque
 
-sys.stdin = open("input.txt")
+# sys.stdin = open("input.txt")
 line = int(input())
+matrix = [list(input()) for _ in range(line)]
+visited = [[0 for _ in range(line)] for _ in range(line)]
 
 # 상 하 우 좌
 dx = [0, 0, 1, -1]
@@ -17,33 +19,45 @@ def iswall(nx,ny): # 벽이 되는 조건
         return False
     if nx >= line or ny >= line: # 좌표가 16을 넘는 경우
         return False
-    if matrix[nx][ny] == 1: # 좌표값이 1인 경우 --> 방문했던 곳은 재방문 x
+    if visited[nx][ny] == 1: # 좌표값이 1인 경우 --> 방문했던 곳은 재방문 x
         return False
     return True
 
 
-def bfs(matrix):
+def bfs(x,y):
     queue = deque()
-    count = 0
-    queue.append((0, 0)) # 출발 지점
-    #matrix[0][0]=1 # 출발 지점을  1 (방문 표시)
+    queue.append((x, y)) # 출발 지점
+    visited[x][y] = 1 # 출발 지점을  1 (방문 표시)
     while queue:
         x,y = queue.popleft() # 좌표
-        if x == line-1 and y == line-1: return count # 도착지점에 도착한 경우
-        compare = matrix[x][y]
-        #matrix[x][y]=1 # 도착 지점에 도착하지 않은 경우 방문 표시
+        visited[x][y]=1 # 도착 지점에 도착하지 않은 경우 방문 표시
         for i in range(4): # 상 하 좌 우 좌표 살피기
             nx,ny=x+dx[i],y+dy[i]
-            if iswall(nx,ny) :
-                if matrix[nx][ny] == compare: # 벽이 없는 경우 &  색이 같은 경우
+            if iswall(nx,ny) : # 막혀있지 않은 경우
+                if matrix[nx][ny] == matrix[x][y]: # 벽이 없는 경우 &  색이 같은 경우
                     queue.append((nx, ny))
-                else :
-                    queue.append((nx,ny))
-                    count +=1
-    return 0
+                    visited[nx][ny] = 1
 
-
+cnt = 0
+# 0 ~ line -1
+for i in range(line):
+    for j in range(line):
+        if visited[i][j]==0: # 방문 안한 경우
+            bfs(i,j)
+            cnt+=1
+print(cnt, end=" ")
 
 # 0 ~ line -1
-matrix = [list(input()) for _ in range(line)]
-print(bfs(matrix))
+for i in range(line):
+    for j in range(line):
+        if matrix[i][j] =='R': # 방문 안한 경우
+            matrix[i][j] ='G'
+visited = [[0 for _ in range(line)] for _ in range(line)]
+cnt = 0
+# 0 ~ line -1
+for i in range(line):
+    for j in range(line):
+        if visited[i][j]==0: # 방문 안한 경우
+            bfs(i,j)
+            cnt+=1
+print(cnt)
