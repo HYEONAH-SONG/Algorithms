d6eb87df50a84ba12ba007f5b9fb6e565c8624fa
unknown <sha082072@gmail.com>
2021-05-01 09:35:03 +0900

    5/1

---
+++ 백준 알고리즘/16234/16234.py
@@ -0,0 +1,6 @@
+import sys
+from collections import deque
+
+sys.stdin = open("input.txt")
+
+N,L,R = map(int,input().split())
--- 백준 알고리즘/백준 15686/15686.py
+++ 백준 알고리즘/백준 15686/15686.py
@@ -1,38 +1,58 @@
 from collections import deque
+from itertools import combinations
+
 import sys
 sys.stdin = open("input.txt")
 
-line, chick = map(int,input().split())
+line, M = map(int,input().split())
 matrix = [list(map(int,input().split())) for _ in range(line)]
 print(matrix)
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+# dx = [0,0,1,-1]
+# dy = [1,-1,0,0]
+#
+# def iswall(x,y):
+#     if matrix[x][y] ==0:
+#         return False
+#     if x<0 or y<0:
+#         return False
+#     if x>=line or y>=line :
+#         return False
+#     return True
 
-def iswall(x,y):
-    if matrix[x][y] ==0:
-        return False
-    if matrix[x][y] ==1:
-        return False
-    if x<0 or y<0:
-        return False
-    if x>=line or y>=line :
-        return False
-    return True
-
-def bfs(x,y):
-    queue = deque((x,y))
-    while queue:
-        x,y = queue.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if iswall(nx,ny): # 2인 경우 (치킨집)
-                if matrix[x][y]==1:
+# def bfs(x,y):
+#     queue = deque((x,y))
+#     while queue:
+#         x,y = queue.popleft()
+#         for i in range(4):
+#             nx, ny = x + dx[i], y + dy[i]
+#             if iswall(nx,ny): # 2인 경우 (치킨집)
+#                 if matrix[nx][ny]==1:
+#                     house.append((nx,ny))
+#                 elif matrix[nx][ny]==2:
+#                     chick.append((nx,ny))
+#                 queue.append((nx,ny))
 
 
-min_num = 0
+chick = []
+house = []
+minv = float('inf')
 for i in range(line):
     for j in range(line):
         if matrix[i][j]==1:
-            min_num+= bfs(i,j)
-print(min_num)
+            house.append((i,j))
+        elif matrix[i][j]==2:
+            chick.append((i, j))
+for ch in combinations(chick,M):
+    sum = 0
+    for h in house:
+        sum +=min([abs(h[0]-i[0])+abs(h[1]-i[1]) for i in chick])
+        if minv <=sum :break
+    if sum < minv : minv =sum
+print(minv)
+
+# sum =0
+# for ch in combinations(chick,M):
+#     for h in house:
+#         sum +=min([abs(h[0]-i[0])+abs(h[1]-i[1]) for i in chick])
+# print(sum)
