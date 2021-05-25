d384aa9e71857060712363d0acbc5f458b71cc28
unknown <sha082072@gmail.com>
2021-05-25 23:10:45 +0900

    5/25

--- 백준 알고리즘/백준 2075/2075.py
+++ 백준 알고리즘/백준 2075/2075.py
@@ -14,18 +14,18 @@ for _ in range(1,N):
     for n in map(int, input().split()):
         heapq.heappush(max_list, n)
         heapq.heappop(max_list)
-print(he)
-
-
-
-reverse_sign = lambda x:x*-1
-for l in reversed(matrix[0:N-1]) :
-    max_list = max_list + l
-stack = []
-for i in range(N):
-    max_list = list(map(reverse_sign, max_list))
-    heapq.heapify(max_list)
-    stack.append(-1 * heapq.heappop(max_list))
-    max_list = list(map(reverse_sign, max_list))
-    heapq.heapify(max_list)
-print(stack.pop())
+# print(he)
+#
+#
+#
+# reverse_sign = lambda x:x*-1
+# for l in reversed(matrix[0:N-1]) :
+#     max_list = max_list + l
+# stack = []
+# for i in range(N):
+#     max_list = list(map(reverse_sign, max_list))
+#     heapq.heapify(max_list)
+#     stack.append(-1 * heapq.heappop(max_list))
+#     max_list = list(map(reverse_sign, max_list))
+#     heapq.heapify(max_list)
+# print(stack.pop())
