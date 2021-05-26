126b02bd875dbe9054457aff8c249e30356c4663
unknown <sha082072@gmail.com>
2021-05-26 21:00:18 +0900

    5/26

--- 백준 알고리즘/백준 2075/2075.py
+++ 백준 알고리즘/백준 2075/2075.py
@@ -1,23 +1,34 @@
+# 표에 적힌 수가 10억보다 적거나 같은 정수 이다.
+# N*N  ---> 시간복잡도 o(N)으로 풀면 된다.
+
 import sys
 import heapq
 # --------------------initialize ---------------
 sys.stdin = open("input.txt")
 N = int(input())
-#matrix = [list(map(int,input().split())) for _ in range(N)]
 max_list = []
 
 # --------------------algorithms-------------------
 max_num = 0
 for n in map(int, input().split()):
-    heapq.heappush(max_list,n)
-for _ in range(1,N):
+    heapq.heappush(max_list,n) # n을 max_list에 맨 마지막 원소로 추가
+# print(max_list)
+for _ in range(N-1): # 총 N 번
     for n in map(int, input().split()):
-        heapq.heappush(max_list, n)
-        heapq.heappop(max_list)
-# print(he)
+        heapq.heappushpop(max_list, n)
+        #heap에 n을 푸시한 다음, heap에서 가장 작은 항목을 pop 한다.
+print(heapq.heappop(max_list))
+
+# import sys
+# import heapq
+# # --------------------initialize ---------------
+# sys.stdin = open("input.txt")
+# N = int(input())
+# matrix = [list(map(int,input().split())) for _ in range(N)]
+# max_list = matrix[4]
 #
-#
-#
+# # --------------------algorithms-------------------
+# max_num = 0
 # reverse_sign = lambda x:x*-1
 # for l in reversed(matrix[0:N-1]) :
 #     max_list = max_list + l
