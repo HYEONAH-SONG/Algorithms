97e616890065c3d5d6d40cf3368f138e31244614
unknown <sha082072@gmail.com>
2021-06-27 01:38:46 +0900

    다익스트라

---
+++ 백준 알고리즘/18352/특정 거리의 도시 찾기.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+sys.stdin = open("input.txt")
+
+N, M, K, X = map(int, input().split()) # 도시의 개수, 도로의 개수, 최단거리 , 출발 도시의 번호
+visited = [0] * (N + 1)
+answer = []
+path = [[] for _ in range(N + 1)]
+for _ in range(M):
+    A, B = map(int, input().split()) # A -> B 로 이동하는 단방향
+    path[A].append(B)
+# ----------------------------------------initialized------------------------------------------#
+
+q = deque()
+q.append((X, 0))
+visited[X]=1
+while q:
+    start, short = q.popleft() # 출발 도시, 최단 거리
+    if short == K:
+        answer.append(start)
+    elif short < K:
+        for i in path[start]:
+            if not visited[i]: # 아직 방문을 안한 경우
+                visited[i] = 1
+                q.append((i, short + 1))
+
+#-----------------------------------BFS---------------------------------------------------------#
+
+# 최단 거리가 k인 도시가 존재하지 않는 경우
+if len(answer) == 0:
+    print(-1)
+else: # 존재하는 경우 --> 오름차순으로 출력
+    answer.sort()
+    for i in answer:
+        print(i)
---
+++ 백준 알고리즘/백준 1753/최단 경로.py
@@ -0,0 +1,52 @@
+# input
+# 5 6 --> 정점의 갯수, 간선의 갯수
+# 1 --> 시작 정점의 번호
+# 5 1 1 --> 각 간선(u -> v, w)
+# 1 2 2
+# 1 3 3
+# 2 3 4
+# 2 4 5
+# 3 4 6
+
+# output
+# 0
+# 2
+# 3
+# 7
+# INF
+
+# 방향 그래프가 주어지면 주어진 시작점에서 다른 모든 정점으로의 최단 경로를 구하는 프로그램을 작성하시오.
+
+import heapq
+import sys
+sys.stdin = open("input.txt")
+V,E = map(int,input().split()) # 정점의 갯수/간선의 갯수
+start_point = int(input()) # 시작 정점의 번호
+
+data =[[] for _ in range(V+1)]
+for _ in range(E):
+    start, end, cost = map(int, input().split())
+    data[start].append((end,cost))
+
+# ---------------------------------------------------------------
+distance = [float('INF') for _ in range(V+1)]
+visited = [0 for _ in range(V+1)]
+
+distance[start_point] = 0
+
+heap = []
+heapq.heappush(heap,(0,start_point))
+
+while heap:
+    dis,location = heapq.heappop(heap)
+    for arrive, cost in data[location]:
+        candidate = dis + cost
+        if candidate<distance[arrive]:
+            distance[arrive]=candidate
+            heapq.heappush(heap,(distance[arrive],arrive))
+
+for i in range(1,V+1):
+    if distance[i] == float('INF'):
+        print("INF")
+    else :
+        print(distance[i])
