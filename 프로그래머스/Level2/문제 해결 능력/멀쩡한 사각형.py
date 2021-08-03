b3662a184cf7a711e9bb19d0ddff1b4fa053cdd1
unknown <sha082072@gmail.com>
2021-08-04 00:39:10 +0900

    8/4

--- 프로그래머스/Level2/문제 해결 능력/멀쩡한 사각형.py
+++ 프로그래머스/Level2/문제 해결 능력/멀쩡한 사각형.py
@@ -1,12 +1,11 @@
 # 사용할 수 있는 정사각형의 갯수 구하기
 # 문제의 창의적인 해결 능력을 보는 문제
+# 한 점이라도 제대로 찍힌 경우에는 사각형 1개 / 아닌 경우 2개
+
+import math
 def solution(w,h):
-    all = w * h
-    if w == h : # 가로와 세로의 길이가 동일한 경우
-        substract = w
-    elif w > h : # 가로가 더 긴 경우
-        substract = h *2
-    else: # 세로가 더 긴 경우
-        substract = w * 2
-    answer = all - substract
-    return answer
+    return w*h - (w+h-math.gcd(w,h))
+
+
+w, h = 8,12
+print(solution(w,h))
