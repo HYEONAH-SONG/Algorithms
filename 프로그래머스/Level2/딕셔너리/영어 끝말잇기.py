e2ac01bf0886aadbbd28c6fc0953865e652aee8c
unknown <sha082072@gmail.com>
2021-08-05 00:45:40 +0900

    8/5

--- 프로그래머스/Level2/딕셔너리/영어 끝말잇기.py
+++ 프로그래머스/Level2/딕셔너리/영어 끝말잇기.py
@@ -1,15 +1,17 @@
+# 시간 복잡도 : O(n) --> n = 100
 def solution(n, words):
-    count = 0
-    d = dict()
+    check = {}
     stack = []
-    division = [i if i != 0 else n for i in range(n+1)]
-    for i in words:
-        count += 1
-        if i in d : # 끝말잇기가 끝나는 조건
-            return [division[count%n],count//n]
-        elif stack and i[0] != stack.pop():
-            return [count % n, count // n]
-        else:
-            stack.append(i[-1])
-            d[i] = 1
+    division = [i if i != 0 else n for i in range(n)] # 나머지
+
+    for count,word in enumerate(words):
+        if word in check : # 이전에 등장했던 단어인 경우
+            return [division[(count+1)%n], count//n+1]
+        elif stack and word[0] != stack.pop(): # 이전 단어의 끝과 현재 단어의 앞이 다른 경우
+            return [division[(count+1)%n], count //n+1]
+        else: # 그 외 진행이 잘 되는 경우
+            stack.append(word[-1])
+            check[word] = 1
     return [0,0]
+n, words = 2, 	["hello", "one", "even", "never", "now", "world", "draw"]
+print(solution(n, words))
---
+++ 프로그래머스/Level2/문제 해결 능력/최솟값 만들기.py
@@ -0,0 +1,12 @@
+# 모든 곱의 합을 다 구하는 것이 옳을까? No --> A의 가장 작은 값과 B의 가장 큰 값을 순서대로 곱해서 더하는 것이 최솟값을 구하는 것
+# 최솟값을 구하는 방법만 알면 순탄하게 풀 수 있는 문제
+# 시간 복잡도 : O(NlogN)
+def solution(A,B):
+    m_sum = 0
+    A.sort() # 오름차순 정렬
+    B.sort(reverse=True) # 내림차순 정렬
+    for i in range(len(A)):
+        m_sum += A[i]*B[i]
+    return m_sum
+A, B =[1, 4, 2],[5, 4, 4]
+print(solution(A,B))
